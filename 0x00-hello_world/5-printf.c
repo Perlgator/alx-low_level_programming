7dffdefb623ce6d2b21f642195c8a9974da184fe
Nelson <chibvomo23@gmail.com>
2023-03-31 07:06:56 -0700

    all

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using printf to print a message
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art//n");
+	return (0);
+}	
