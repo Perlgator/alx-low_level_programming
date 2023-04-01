6e63ae92abc07c23e1e869ebcc601c80d13e69e8
Nelson <chibvomo23@gmail.com>
2023-04-01 05:09:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - C program that prints a message followed by a new line
  *
- * Return: 0
+ *Return: 0
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
