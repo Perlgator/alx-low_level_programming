9dff77eb2293f1b0f2c0e61a6eaf424df863a68d
Perlgator <chibvomo23@gmail.com>
2023-08-27 06:44:56 -0700

    redone

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _puts - prints a string of char
+ *@s: pointer to string
+ * Return: void
+ */
 void _puts(char *s)
 {
-    puts(s);
+	puts(s);
 }
